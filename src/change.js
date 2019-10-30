c4efc8b3969c34725542e8054537fd923e274fe3
Alexander Gianakouros <agianakouros@ucvts.org>
2019-10-30 10:34:16 -0400

    all period for just the first problem

--- src/change.js
+++ src/change.js
@@ -3,12 +3,28 @@ const readlineSync = require("readline-sync");
 const MIN = 0.01;
 const MAX = 0.99;
 
+const QUARTER = 25;
+const DIME = 10;
+const NICKEL = 5;
+
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("\nInvalid")
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("\nInvalid")
 } else {
-    // write your code for making change here
+
+let whole = (amount * 100);
+
+let numOfQuarters = Math.floor(whole / QUARTER);
+let change1 = (whole % QUARTER);
+
+let numOfDimes = Math.floor(change1 / DIME);
+let change2 = (change1 % DIME);
+
+let numOfNickels = Math.floor(change2 / NICKEL);
+let change3 = (change2 % NICKEL);
+
+console.log("\n" + numOfQuarters +  " quarters, " + numOfDimes + " dimes, " + numOfNickels + " nickels, and " + change3 + " pennies.");
 }
