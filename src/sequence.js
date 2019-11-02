ffdb5982c25ceb34f66045a10e3783668c74aca7
Alex G <agianakouros@ucvts.org>
2019-11-02 14:26:39 -0400

    weekend grind

--- src/sequence.js
+++ src/sequence.js
@@ -6,23 +6,45 @@ const MAX = Number.MAX_SAFE_INTEGER;
 
 console.log("\nEnter three numbers.");
 
-const a = Number(readlineSync.question("\n"));
-const b = Number(readlineSync.question(""));
-const c = Number(readlineSync.question(""));
+let a = Number(readlineSync.question("\n"));
+let b = Number(readlineSync.question(""));
+let c = Number(readlineSync.question(""));
 
 
 if (Number.isNaN(a)) {
     console.log("\nInvalid.")
   } else if (a < MIN || a > MAX) {
     console.log("\nInvalid.")
-  } if (Number.isNaN(b)) {
+  } else if (Number.isNaN(b)) {
     console.log("\nInvalid.")
   } else if (b < MIN || b > MAX) {
     console.log("\nInvalid.")
-  } if (Number.isNaN(c)) {
+  } else if (Number.isNaN(c)) {
     console.log("\nInvalid.")
   } else if (c < MIN || c > MAX) {
     console.log("\nInvalid.")
-  }
 
-  
+
+  } else {
+
+    if ((a === b) && (b === c)) {
+    console.log("\nEqual.")
+  } else if ((a === b) && (b < c)) {
+    console.log("\nIncreasing.")
+  } else if ((a < b) && (b === c)) {
+    console.log("\nIncreasing.")
+  } else if ((a < b) && (b < c)) {
+    console.log("\nStrictly increasing.")
+  } else if ((a > b) && (b > c)) {
+    console.log("\nStrictly decreasing.")
+  } else if ((a === b) && (b > c)) {
+    console.log("\nDecreasing.")
+  } else if ((a > b) && (b === c)) {
+    console.log("\nDecreasing.")
+  } else if ((a > b) && (b < c)) {
+    console.log("\nUnordered.")
+  } else if ((a < b) && (b > c)) {
+    console.log("\nUnordered.")
+  } else {
+    console.log("\nDecreasing.")
+  }  }
